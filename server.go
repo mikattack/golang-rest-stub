ff6c8d55104b2c801cb61d57213a454a8200197a
Alex Mikitik <alex@mikitik.com>
2015-05-24 17:06:13 -0700

    Implement application behavior.

--- server.go
+++ server.go
@@ -1,23 +1,54 @@
 package main
 
 import (
+  "bytes"
   "fmt"
   "log"
   "net/http"
+  "strconv"
+  "time"
 )
 
 
-/* 
- * Start the HTTP server.
- */
+func handleRequest(res http.ResponseWriter, req *http.Request) {
+  var delay time.Duration = 0
+  status := 200
+  writer := bytes.Buffer{}
+
+  // Delay response, when requested
+  if val := req.Header.Get("X-Stub-Delay"); val != "" {
+    amount, err := strconv.ParseInt(val, 10, 64)
+    if err != nil {
+      log.Printf("[%s]: Cannot interpret 'X-Stub-Delay' header value (%s) as integer\n", req.URL, val)
+    } else {
+      delay = time.Duration(amount) * time.Millisecond
+    }
+  }
+
+  // Determine appropriate response status
+  if val := req.Header.Get("X-Stub-Status"); val != "" {
+    parsed, err := strconv.ParseInt(val, 10, 0)
+    if err != nil || parsed < 100 || parsed >= 600 {
+      log.Printf("[%s]: Invalid 'X-Stub-Status' value (%s)\n", req.URL, val)
+    } else {
+      status = int(parsed)
+    }
+  }
+
+  // Use stub content for response body, when possible
+
+  // Respond
+  time.Sleep(delay)
+  res.WriteHeader(status)
+  if _, err := writer.WriteTo(res); err != nil {
+    log.Printf("[%s]: Failed to write response\n", req.URL)
+  }
+}
+
+
 func main() {
   config, _ := getConfig()
-  router := httprouter.New()
-
   fmt.Printf("Starting HTTP server on port [%s]\n", config["port"])
-
-  // Start a server to listen for JSON-encoded checkmk queries via HTTP
-  fmt.Printf("Starting HTTP server on port [%s]\n", config["port"])
-  http.HandleFunc("/", newStubHandler(config))
+  http.HandleFunc("/", handleRequest)
   http.ListenAndServe(":" + config["port"], nil)
 }

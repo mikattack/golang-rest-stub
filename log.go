b43c5dac0102abcbb891868b53174c348639bad8
Alex Mikitik <alex@mikitik.com>
2015-10-26 06:48:32 -0700

    Update logger to log only to STDOUT and support log levels

--- log.go
+++ log.go
@@ -1,50 +1,33 @@
 package main
 
 import (
-  "fmt"
-  "io"
   "log"
   "os"
+  "github.com/hashicorp/logutils"
 )
 
 
 /* 
- * Detects how the application should log based on runtime configuration
- * and sets the global logger appropriately.
+ * All logging is done to STDOUT.
  * 
- * By default, the logger logs to a file as set by the "log" configuration
- * directive.  If that directive is an empty string, then file logging is
- * disabled.
+ * Logging levels are currently supported, but currently not configurable.
+ * A log entry's level is indicated within the log message itself with
+ * a pattern like this: [LEVEL].
  * 
- * If the debugging has been enabled, the logger will additionally log
- * to STDOUT.  It is possible to enable debug output with no file logging.
+ * Examples:
+ *  // Assuming minimum level is 'WARN'
+ *  log.Print("[DEBUG] Debugging")  // Does not print
+ *  log.Print("[WARN] Warning")     // Prints
+ *  log.Print("[ERROR] Erring")     // Prints
  * 
- * Logging levels are not currently supported.
+ * Currently, the following levels are supported: DEBUG, WARN, ERROR.
  */
 func init() {
-  config, debug := getConfig()
-  writers := []io.Writer{}
-
-  // Enable file logging (when possible)
-  // NOT WORKING - Disabled for now
-  if false && config["log"] != "" {
-    fmt.Printf("Logging to file: %s\n", config["log"])
-    fd, err := os.OpenFile(config["log"], os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0600)
-    if err != nil {
-      fmt.Printf("File logging disabled, unable to access file\n")
-      fmt.Println(err.Error())
-    } else {
-      writers = append(writers, fd)
-    }
-    defer fd.Close()
+  filter := &logutils.LevelFilter{
+    Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+    MinLevel: logutils.LogLevel("WARN"),
+    Writer:   os.Stdout,
   }
-
-  // Enable STDOUT debug logging (if requested)
-  if debug {
-    fmt.Println("Debugging enabled")
-    writers = append(writers, os.Stdout)
-  }
-
-  log.SetOutput(io.MultiWriter(writers...))
-  log.SetFlags(log.LstdFlags | log.Lshortfile)
+  log.SetOutput(filter)
+  log.SetFlags(log.LstdFlags)
 }
